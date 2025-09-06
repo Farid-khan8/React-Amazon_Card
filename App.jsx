f0232d819bf057b0fb97d5da13858b289e1c13fb
Fariduddin Khan <149661962+Farid-khan8@users.noreply.github.com>
2025-09-07 00:47:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Product from "./Product.jsx";
+import ProductTab from "./Producttab.jsx";
+import MsgBox from "./MsgBox.jsx";
+
+function App() {
+    return (
+        <div>
+            <h2>BLockBuster Deals | Shop Now</h2>
+            <ProductTab />
+            {/* <Title /> */}
+            {/* <Product /> */}
+            {/* <MsgBox username="Rohan" textcolor="pink" /> */}
+        </div>
+    );
+}
+
+export default App;
